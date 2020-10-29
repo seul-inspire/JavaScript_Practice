883be3b9fab061def7ca94fbf8bb317f6582d25c
seulgi_lee <31645464+seul-inspire@users.noreply.github.com>
2020-10-29 23:05:02 +0900

    삼항연산자 사용하여 10보다 작을 경우에 2자리로 맞춰줌

--- 2-Clock/js/clock.js
+++ 2-Clock/js/clock.js
@@ -2,12 +2,13 @@ const clockContainer = document.querySelector('.js-clock'),
       colockTitle = clockContainer.querySelector('h1');
 
 function getTime() {
-  let now = new Date();
-  let minutes = now.getMinutes();
-  let hours = now.getHours();
-  let seconds = now.getSeconds();
-  colockTitle.innerText = `${hours}:${minutes}:${seconds}`;
-}
+  const now = new Date(),
+        minutes = now.getMinutes(),
+        hours = now.getHours(),
+        seconds = now.getSeconds();
+  colockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+}//end of getTime()
 function init() {
   setInterval(getTime, 1000);
 }
