52208f56ded23fe72274f54f587a7f950021e747
seulgi_lee <31645464+seul-inspire@users.noreply.github.com>
2020-10-30 00:33:06 +0900

    이름 값 받기 및 인사말 출력
    localStorage 이용

--- 2-Clock/js/clock.js
+++ 2-Clock/js/clock.js
@@ -1,13 +1,13 @@
 const clockContainer = document.querySelector('.js-clock'),
-      colockTitle = clockContainer.querySelector('h1');
+      colockTitle = clockContainer.querySelector('p');
 
 function getTime() {
   const now = new Date(),
         minutes = now.getMinutes(),
         hours = now.getHours(),
         seconds = now.getSeconds();
+  //시,분,초 10보다 작으면 앞에 0을 붙여서 2자리로 맞춰줌
   colockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
-
 }//end of getTime()
 function init() {
   setInterval(getTime, 1000);
